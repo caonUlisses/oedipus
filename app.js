9d7efdb318449708c53945e058cdc2aed7c905fa
Ulisses Caon <ulissescaon@gmail.com>
2017-11-25 12:05:17 -0200

    :hammer: Refactor patch route to async/await | Format code

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const express    = require('express')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 
-const {User}         = require('./server/models/user')
-const {authenticate} = require('./server/middleware/authenticate')
-const {mongoose}     = require('./server/db/mongoose')
-const {ObjectId}     = require('mongodb')
-const {picture}      = require('./server/utils/picture')
+const { User }         = require('./server/models/user')
+const { authenticate } = require('./server/middleware/authenticate')
+const { mongoose }     = require('./server/db/mongoose')
+const { ObjectId }     = require('mongodb')
+const { picture }      = require('./server/utils/picture')
 
 const app = express()
 
@@ -24,78 +24,59 @@ app.get('/users/', authenticate, async (req, res) => {
   try {
     const users = await User.find()
 
-    if(!users) {
-      throw new error({message: 'Houve um erro na página'})
+    if (!users) {
+      throw new error({ message: 'Houve um erro na página' })
     }
     res.status(200).send(users)
-  } catch(error) {
-    res.status(400).send({message: "Houve um erro", error})
+  } catch (error) {
+    res.status(400).send({ message: "Houve um erro", error })
   }
 })
 
 app.post('/users/', async (req, res) => {
-  try{
-    let   body   = _.pick(req.body, ['name','email', 'password'])
-    body.picture = picture.preparePicture(req)
-    let   access = req.body.access ? req.body.access : 'admin'
-    let   user   = await new User(body)
+  try {
+    let body         = _.pick(req.body, ['name', 'email', 'password'])
+        body.picture = picture.preparePicture(req)
+    let access       = req.body.access ? req.body.access : 'admin'
+    let user         = await new User(body)
     await user.save()
-    const token  = await user.generateAuthToken(access)
+    const token = await user.generateAuthToken(access)
 
-    res.header('x-auth', token).status(200).send({message: "Usuário criado com sucesso"})
+    res.header('x-auth', token).status(200).send({ message: "Usuário criado com sucesso" })
   } catch (error) {
-    res.status(400).send({message: "Houve um erro", error})
+    res.status(400).send({ message: "Houve um erro", error })
   }
 })
 
-app.patch('/users/:_id', authenticate, (req, res) => {
-  let _id     = req.params._id
-  let hash    = Math.random().toString(36).substring(7)
-  let picture = req.files ? req.files.picture : null
-  
-  let pictureName = picture ? hash + picture.name : "default.png"
-  let picturePath = `./files/${pictureName}`
-  
-  if(picture) {
-    picture.mv(`./files/${pictureName}`, (err) => {
-      if(err) {
-        return res.send(err)
-      }
-    })
-  }
-  
-  let body     = _.pick(req.body, ['name','email', 'password'])
-  body.picture = picturePath
-  
-  let access      = req.body.access ? req.body.access : null
-  
-  User.findOneAndUpdate({_id}, { $set: body }, { new: true }).then((user) => {
-    let token     = req.token
-    let userToken = user.tokens[0].token
-    if(!user) {
-      return res.status(404).send({message: 'Houve um problema localizando o Usuário'})
+app.patch('/users/:_id', authenticate, async (req, res) => {
+  try {
+    const _id          = req.params._id
+    const body         = _.pick(req.body, ['name', 'email', 'password'])
+          body.picture = picture.preparePicture(req)
+    const user         = await User.findOneAndUpdate({ _id }, { $set: body }, { new: true })
+    const token        = req.token
+    const userToken    = user.tokens[0].token
+
+    if (userToken !== token) {
+      return res.status(401).send({ message: 'Você não pode alterar este usuário' })
     }
 
-    if(userToken !== token ) {
-      return res.status(401).send({message: 'Você não pode alterar este usuário', token, userToken})
-    }
-    
-    res.send({user})
-  }).catch((e) => {
-    res.status(400).send({message: 'Houve um problema na alteração do Usuário'})
-  })
+    res.send({ user })
+  } catch (e) {
+    res.status(400).send({ message: 'Houve um problema na alteração do Usuário' })
+  }
 })
 
 app.get('/users/:id', authenticate, async (req, res) => {
-  const id   = req.params.id
-  
+  const id = req.params.id
+
   try {
     const user = await User.findById(id)
-    if(!user) {
-      res.status(400).send({message: 'Não deu certo'})
+    if (!user) {
+      res.status(400).send({ message: 'Não deu certo' })
     }
     res.status(200).send(user)
-  } catch(error) {
+  } catch (error) {
     res.status(400).send(e)
   }
 })
@@ -104,8 +85,8 @@ app.delete('/users/:id', authenticate, async (req, res) => {
   const id = req.params.id
   try {
     const user = await User.findByIdAndRemove(id)
-    res.status(200).send({user})
-  } catch(e) {
+    res.status(200).send({ user })
+  } catch (e) {
     res.status(400).send(e)
   }
 })
@@ -116,14 +97,14 @@ app.get('/auth', async (req, res) => {
   try {
     const user = await User.returnByToken(token)
     res.status(200).send(user)
-  } catch(e) {
+  } catch (e) {
     res.status(401).send('Não foi possível localizar o usuário')
   }
 })
 
 app.use((req, res, next) => {
   const err        = new Error('Página não encontrada')
-  err.status = 404;
+        err.status = 404;
   next(err)
 })
 
@@ -135,4 +116,4 @@ app.use((err, req, res, next) => {
   res.send(err)
 })
 
-module.exports = {app}
+module.exports = { app }
