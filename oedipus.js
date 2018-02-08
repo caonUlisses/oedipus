732bb1313bae26b1720243b6bf0206837fa286a7
Ulisses Caon <ulissescaon@gmail.com>
2018-02-08 20:31:30 -0200

    Add admin and sudo middleware

--- oedipus.js
+++ oedipus.js
@@ -2,6 +2,8 @@
 
 const { oedipus } = require('./server/routes/users.js')
 const { authenticate } = require('./server/middleware/authenticate.js')
+const { admin } = require('./server/middleware/admin.js')
+const { sudo } = require('./server/middleware/sudo.js')
 
 module.exports = {
   oedipus,
---
+++ server/middleware/admin.js
@@ -0,0 +1,27 @@
+const { User } = require('./../models/user.js')
+
+const admin = async (req, res, next) => {
+  try {
+    const token = req.header('x-auth')
+    const user = await User.findByToken(token)
+
+    if (!token) {
+      return res.status(401).send({message: 'Faça login primeiro'})
+    }
+
+    if (!user) {
+      return res.status(400).send({message: 'Usuário não encontrado'})
+    }
+
+    if (user.tokens.access !== 'admin') {
+      return res.status(401).send({message: 'Você não tem permissão para acessar este recurso'})
+    }
+
+    req.token = token
+    next()
+  } catch (error) {
+    res.status(401).send({message: 'Usuário não encontrado'})
+  }
+}
+
+module.exports = { admin }
